0c6d92898a5366931aa841814eed58fa0ccdb15a
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-24 14:36:16 -0300

    feat(requisito-06): adiciona requisito completo.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    all_news = search_news({'title': {'$regex': title, '$options': 'i'}})
+    data_new = []
+
+    for new in all_news:
+        data_new.append((new['title'], new['url']))
+
+    return data_new
 
 
 # Requisito 7
