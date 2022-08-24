e1135b5caa52c806315ea306a0054fc0e38db5c7
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-24 15:16:27 -0300

    feat(requisito-07): adiciona requisito completo.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tech_news.database import search_news
 
 
@@ -5,6 +6,7 @@ from tech_news.database import search_news
 def search_by_title(title):
     """Seu código deve vir aqui"""
     all_news = search_news({'title': {'$regex': title, '$options': 'i'}})
+    # print(all_news)
     data_new = []
 
     for new in all_news:
@@ -16,6 +18,16 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    date_new = []
+    try:
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
+        date_find = datetime.strftime(date_obj, '%d/%m/%Y')
+        all_date = search_news({'timestamp': date_find})
+        for new in all_date:
+            date_new.append((new['title'], new['url']))
+    except ValueError:
+        raise ValueError('Data inválida')
+    return date_new
 
 
 # Requisito 8
